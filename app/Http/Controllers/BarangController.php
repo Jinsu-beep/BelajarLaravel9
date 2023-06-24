e7e2dc60b60432cf21df87be738a70786632fceb
tyagi <79263901+Jinsu-beep@users.noreply.github.com>
2023-06-25 03:02:04 +0800

    update VarangController

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Barang;
+use App\Models\Jenis;
+
 class BarangController extends Controller
 {
     //
